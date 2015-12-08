6ed083688923217fdc296fe6f585608e91853ac1
zomars <zomars@me.com>
2015-12-07 19:17:29 -0700

    Code refactoring

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/');
+    this.nicknames = ko.observableArray(['Pelos', 'Don gato', 'Padrino', 'Pelusa']);
+
+    this.title = ko.computed(function(){
+        var title;
+        var clicks = this. clickCount();
+        if (clicks < 10) {
+            title = 'Newborn';
+        } else if (clicks < 20) {
+            title = 'Infant';
+        } else if (clicks < 30) {
+            title = 'Child';
+        } else if (clicks < 40) {
+            title = 'Teen';
+        } else if (clicks < 50) {
+            title = 'Adult';
+        } else {
+            title = 'Ninja';
+        }
+        return title;
+    }, this);
+}
+
+var ViewModel = function() {
+
+    this.currentCat = ko.observable( new Cat() );
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     };
 }
 
